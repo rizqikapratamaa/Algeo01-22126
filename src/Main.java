7153b39b2905c914aa3d4aa1dd009c85b82fb692
rizqikapratamaa <rizqikapratamaa@gmail.com>
2023-09-20 21:33:49 +0700

    bikin main

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/*
+Ini buat main program utamanya
+Inputtan masuk ke variabel input
+*/
+
+public class Main {
+    static Scanner in = new Scanner (System.in);
+    public static void main(String[] args) {
+        boolean jalan = true;
+        int input = 0;
+        String line;
+        String[] row = new String[100];
+    }
+
+}
